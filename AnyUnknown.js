a5adf42d8a2590a5c4b5c1f05e6ca90447315ab0
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2024-11-15 15:16:28 +0530

    unknown and any , unknown use in api concept

--- AnyUnknown.js
+++ AnyUnknown.js
@@ -7,3 +7,16 @@ console.log("num.........");
 console.log(num);
 num = "6";
 console.log(num);
+let value;
+value = 4;
+value = true;
+value = "jyoti";
+if (typeof value === "number") {
+    console.log(value + 10);
+}
+else if (typeof value === "boolean") {
+    console.log("boolean = " + value);
+}
+else {
+    console.log("string = " + value);
+}
