706f201af2f6891b336a688dc063be16184eabed
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2024-11-15 15:04:56 +0530

    through any data type can be any value store in variable like js concept

---
+++ AnyUnknown.js
@@ -0,0 +1,9 @@
+"use strict";
+const numberBig = Number.MAX_SAFE_INTEGER;
+console.log("number Big");
+console.log(numberBig);
+let num = 5;
+console.log("num.........");
+console.log(num);
+num = "6";
+console.log(num);
---
+++ AnyUnknown.ts
@@ -0,0 +1,11 @@
+const numberBig = (Number as any).MAX_SAFE_INTEGER
+console.log("number Big")
+console.log(numberBig)
+
+
+
+let num:any = 5
+console.log("num.........");
+console.log(num)
+num = "6"
+console.log(num)
